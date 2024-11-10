af9735293dbef5574bb1a337cdca0498f1cb34ae
Smile Gupta <smile.gupta@razorpay.com>
2024-11-10 08:58:53 +0530

    feat: added some change in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
-import LanguageSwitcher from "./LanguageSwitcher";
+import { Link } from "react-router-dom";
+
+// ui imports
+import LanguageSwitcher from "src/components/LanguageSwitcher";
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center bg-blue-600 p-4 md:p-6 text-white shadow-md">
-      <h1 className="text-3xl font-semibold tracking-wide hover:text-blue-200 transition duration-200 ease-in-out" aria-label="application Name">
-        Meditrace
-      </h1>
+      <Link to="/">
+        <h1
+          className="text-3xl font-semibold tracking-wide hover:text-blue-200"
+          aria-label="application name - meditrace"
+        >
+          Meditrace
+        </h1>
+      </Link>
       <LanguageSwitcher />
     </header>
   );
